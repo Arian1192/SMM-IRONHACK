430fdbe0b40647d695061c39db3a8323780fd59e
Arian <arian.collaso.rodriguez@gmail.com>
2024-04-13 13:10:00 +0200

    Pre-Work: Create Repository with Spring Initializer

---
+++ src/main/java/com/ssm/systemmeetmanagement/SystemMeetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ssm.systemmeetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemMeetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemMeetManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssm/systemmeetmanagement/SystemMeetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssm.systemmeetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemMeetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
